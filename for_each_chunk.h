36a67198bd586c7d3c4df3888423cb65cff93087
Florian Paul Schmidt <mista.tapas@gmx.net>
2013-03-04 13:55:18 +0100

    initial checkin

---
+++ for_each_chunk.h
@@ -0,0 +1,30 @@
+#ifndef FOR_EACH_CHUNK_INCLUDED_HH
+#define FOR_EACH_CHUNK_INCLUDED_HH
+
+#include <iostream>
+
+/**
+ * Iterator must be a random access iterator
+ */
+template<class Iterator, class Functor>
+inline void for_each_chunk(Iterator begin, Iterator end, unsigned int chunk_size, Functor f) {
+	unsigned int num_of_chunks = (end - begin) / chunk_size;
+	unsigned int remainder = (end - begin) % chunk_size;
+	
+	// std::cout << "# of chunks: " << num_of_chunks << std::endl;
+	
+	/*
+	 * Handle "normal" chunks
+	 */
+	for (unsigned int index = 0; index < num_of_chunks; ++index)
+	{
+		f(begin + chunk_size * index, begin + chunk_size * (index + 1));
+	}
+	
+	/*
+	 * Handle remainder
+	 */
+	f(begin + chunk_size * num_of_chunks, end);
+}
+
+#endif
---
+++ test_for_each_chunk.cc
@@ -0,0 +1,29 @@
+#include "for_each_chunk.h"
+
+#include <vector>
+#include <iostream>
+
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+
+void print(std::vector<int>::iterator begin, std::vector<int>::iterator end) 
+{
+	std::cout << "chunk" << std::endl;
+	
+	for (std::vector<int>::iterator it = begin; it != end; ++it)
+	{
+		std::cout << "an element: " << (*it) << std::endl;
+	}
+}
+
+int main() 
+{
+	std::vector<int> v;
+	
+	for (unsigned int index = 0, max = 1010; index < max; ++index) 
+	{
+		v.push_back(index);
+	}
+		
+	for_each_chunk(v.begin(), v.end(), 100, print);
+}
